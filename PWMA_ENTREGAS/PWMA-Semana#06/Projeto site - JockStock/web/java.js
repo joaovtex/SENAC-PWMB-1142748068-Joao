47f063c576cf21ca3c81704be7ccd6f69fbcb963
joao <joaovictorfarias1610@gmail.com>
2023-09-21 22:02:15 -0300

    semana 6.1

---
+++ PWMA_ENTREGAS/PWMA-Semana#06/Projeto site - JockStock/web/java.js
@@ -0,0 +1,13 @@
+
+        
+        function logar() {
+            let nome = window.prompt('Qual é o seu nome?')
+            let res = window.document.getElementById('resultado')
+            
+            res.innerHTML = `<p>Olá, ${nome}`
+            
+        }
+
+
+ 
+        
